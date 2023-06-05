d1636e21e78934563d2f7a41c878b5bcdeaa28e8
Garingor <36760314+Garingor@users.noreply.github.com>
2023-06-05 03:31:04 +0300

    delete lab_04

--- data types and structures (3 semester)/lab_04/menu.h
+++
@@ -1,5 +0,0 @@
-#ifndef LAB_04_MENU_H
-#define LAB_04_MENU_H
-void print_menu();
-void get_choice(int, int, int *);
-#endif //LAB_04_MENU_H
--- data types and structures (3 semester)/lab_04/utills.h
+++
@@ -1,6 +0,0 @@
-#include "const.h"
-#ifndef LAB_04_UTILLS_H
-#define LAB_04_UTILLS_H
-int calculation(node **, double **, int *, int *, char *, int);
-void time_calc();
-#endif //LAB_04_UTILLS_H
